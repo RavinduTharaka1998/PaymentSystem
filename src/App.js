c2fc34704fe1beceb49b07e7d636c77aa3ee6aec
RavinduTharaka1998 <68723931+RavinduTharaka1998@users.noreply.github.com>
2023-04-11 08:07:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link, BrowserRouter} from 'react-router-dom';
+
+
+
+
+
+class App extends Component{
+
+  render() {
+    return(
+        <div>
+                <Router>
+                    <Switch>
+                       
+                    </Switch>
+                </Router>
+
+        </div>
+    );
+  }
+}
+
+export default App;
